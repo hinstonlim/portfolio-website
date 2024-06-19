4ed9c250fed1c839604aea125b17617b3a1cf592
Hinston Lim <hinstonlimzx@gmail.com>
2024-06-19 20:37:44 +0800

    set theme to dark by default

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -33,18 +33,20 @@ export default function ThemeContextProvider({
   };
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    // const localTheme = window.localStorage.getItem("theme") as Theme | null;
 
-    if (localTheme) {
-      setTheme(localTheme);
+    // if (localTheme) {
+    //   setTheme(localTheme);
 
-      if (localTheme === "dark") {
-        document.documentElement.classList.add("dark");
-      }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-      document.documentElement.classList.add("dark");
-    }
+    //   if (localTheme === "light") {
+    //     document.documentElement.classList.add("light");
+    //   } else {
+    //     document.documentElement.classList.add("dark");
+    //   }
+    // } else {
+    setTheme("dark");
+    document.documentElement.classList.add("dark");
+    // }
   }, []);
 
   return (
