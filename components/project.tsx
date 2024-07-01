f0bdd7372f1fb482b4999e7829e06ce10908e9fb
Hinston Lim <hinstonlimzx@gmail.com>
2024-07-01 18:23:06 +0800

    - Allow project screenshots to be visible when in phone mode

--- components/project.tsx
+++ components/project.tsx
@@ -30,7 +30,7 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[52rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:min-h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      <section className="bg-gray-100 max-w-[52rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:min-h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20 sm:block flex flex-col items-center">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col justify-between h-full sm:group-even:ml-[18rem]">
           <div className="pb-4">
             <h3 className="text-2xl font-semibold">{title}</h3>
@@ -54,7 +54,7 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          className="sm:absolute sm:block top-8 -right-40 w-[28.25rem] rounded shadow-2xl
         transition 
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
